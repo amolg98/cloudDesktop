07f09de75829ec2238ae5763d5d0f461b51c78b8
agupta <amol.gupta@us.sios.com>
2021-04-18 15:09:48 -0400

    Initial commit

---
+++ src/base_cloud.py
@@ -0,0 +1,21 @@
+import os
+
+class BaseCloud(object):
+    """
+
+    """
+    def __init__(self, id):
+        self.id = id
+
+    def cloud_credentials():
+        pass
+
+    def cloud_vms():
+        pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main
